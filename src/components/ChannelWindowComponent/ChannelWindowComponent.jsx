f41e378e21cdebe2dab4508b31fb559060aa24da
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-12-10 13:37:00 +0530

    Add. Included the ChannelWindow layout using local json.

--- src/components/ChannelWindowComponent/ChannelWindowComponent.jsx
+++ src/components/ChannelWindowComponent/ChannelWindowComponent.jsx
@@ -1,13 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoFilter } from "react-icons/io5";
 import { HiOutlinePencilAlt } from "react-icons/hi";
-import { FaChevronDown } from "react-icons/fa";
+import { FaChevronDown, FaLock, FaChevronRight } from "react-icons/fa";
+import { FaHashtag } from "react-icons/fa6";
+import { Collapse } from 'react-collapse'
+import data from '../../data/channelListData'
+
 
 
 
 const ChannelWindowComponent = () => {
+
+    const [isOpenedChannel, setIsOpenedChannel] = useState(true)
+    const [isOpenedDm, setIsOpenedDm] = useState(true)
+
+    const handleCollapseChannels = () => {
+        setIsOpenedChannel(!isOpenedChannel)
+    }
+
+    const handleCollapseDm = () => {
+        setIsOpenedDm(!isOpenedDm)
+    }
+
     return (
-        <div className="channel-window">channel</div>
+        <div className="channel-window">
+            <header>
+                <div className="title-container">
+                    <h3 className='title'>Saveetha Engineering College</h3>
+                    <span><FaChevronDown/></span>
+                </div>
+                <span><IoFilter/></span>
+                <span><HiOutlinePencilAlt/></span>
+
+            </header>
+
+            <main>
+                <div className="channel__thread">
+
+                </div>
+
+                <div className="channel__channels">
+                    <div 
+                        className="channels-title"
+                        onClick={handleCollapseChannels}
+                    >
+                        <span>
+                            { isOpenedChannel ? 
+                                    <FaChevronRight/> 
+                                :
+                                    <FaChevronDown/>
+                            }
+                        </span>
+
+                        <h3>Channel</h3>
+                    </div>
+                    <Collapse isOpened={isOpenedChannel}>
+                        { data && data.map( (channel, index) => (
+                            <div
+                                className="channels-list"
+                                key={index}
+                            >
+                                <span><FaHashtag/></span>
+                                <li>{channel.channelTitle}</li>
+                            </div>
+                        ))}
+                    </Collapse>
+                </div>
+
+                <div className="channel__dm">
+
+                    <div 
+                        className="channels-title"
+                        onClick={handleCollapseDm}
+                    >
+                        <span>
+                            { isOpenedDm ? 
+                                    <FaChevronRight/> 
+                                :
+                                    <FaChevronDown/>
+                            }
+                        </span>
+
+                        <h3>Direct message</h3>
+                    </div>
+
+                    <Collapse isOpened={isOpenedDm}>
+                        { data && data[0].members.map( (channel, index) => (
+                            <div
+                                className="channels-list"
+                                key={index}
+                            >
+                                <span><FaHashtag/></span>
+                                <li>{channel}</li>
+                            </div>
+                        ))}
+                    </Collapse>
+                </div>
+            </main>
+        </div>
     )
 }
 
---
+++ src/data/channelListData.js
@@ -0,0 +1,25 @@
+const channelList = [
+    {
+        id : 101,
+        channelTitle : "general",
+        visible : "public",
+        owner : "common",
+        members : ["Thiru", "Arun", "Bala", "Prabanjan", "Sethu"]
+    },
+    
+    {
+        id : 102,
+        channelTitle : "random",
+        visible : "public",
+        owner : "common"
+    },
+    
+    {
+        id : 103,
+        channelTitle : "TechTenacity",
+        visible : "public",
+        owner : "thiru"
+    }
+]
+
+export default channelList
